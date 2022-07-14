f48819bd9399c648808b7089d16c1eb6b084d9c7
navigaid <navigaid@gmail.com>
2022-07-14 23:50:43 +0800

    solution1

--- lib.rs
+++ lib.rs
@@ -9,7 +9,59 @@ pub fn add(a: i32, b: i32) -> i32 {
 /// get string length
 #[wasm_bindgen]
 pub fn strlen(s: &str) -> usize {
+  println!("{}", "haha");
   return s.len();
+}
+
+mod oneblock {
+  mod solution1 {
+    use super::super::*;
+
+    #[derive(Debug)]
+    enum TrafficLight {
+      Red,
+      Green,
+      Yellow,
+    }
+
+    trait Light {
+      fn duration(&self) -> i32;
+    }
+
+    impl Light for TrafficLight {
+      fn duration(&self) -> i32 {
+        return match self {
+          TrafficLight::Red => 30,
+          TrafficLight::Green => 5,
+          TrafficLight::Yellow => 45,
+        };
+      }
+    }
+
+    fn display_light<T>(light: T) -> String
+    where
+      T: Light + std::fmt::Debug,
+    {
+      format!("{:?} light lasts for {}s", light, light.duration())
+    }
+
+    macro_rules! _display_light {
+      ($( $light:expr),* $(,)?) => {{
+        $( println!("{:?} light lasts for {}s", $light, $light.duration()); )*
+      }};
+    }
+
+    /// show traffic lights
+    #[wasm_bindgen]
+    pub fn solution1() -> String {
+      [
+        display_light(TrafficLight::Red),
+        display_light(TrafficLight::Green),
+        display_light(TrafficLight::Yellow),
+      ]
+      .join("\n")
+    }
+  }
 }
 
 #[cfg(test)]
--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 import { instantiate } from "./mod.ts";
 
-const { add, strlen } = await instantiate();
+const { add, strlen, solution1 } = await instantiate();
 
 console.log(add(1, 2));
 
 console.log(strlen("btwiuse"));
+
+console.log(solution1());

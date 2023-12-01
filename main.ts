e770ea61066e57d4b52279748d40e369171b8ebf
btwiuse <54848194+btwiuse@users.noreply.github.com>
2023-12-01 19:24:54 +0000

    update js caller

--- main.ts
+++ main.ts
@@ -2,26 +2,6 @@ import { instantiate } from "./mod.ts";
 
 const {
   add,
-  counter,
-  incr,
-  strlen,
-  solution1,
-  solution2,
-  solution3,
 } = await instantiate();
 
 console.log(add(1, 2));
-
-console.log(strlen("btwiuse"));
-
-console.log(solution1());
-
-console.log(solution2());
-
-console.log(solution3());
-
-console.log("counter:", counter());
-for (let i = 0; i <= 3; i++) {
-  incr(50);
-  console.log("counter:", counter());
-}

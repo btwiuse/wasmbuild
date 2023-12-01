49610b908090a522c21f9fe95c75a01b99dd7383
btwiuse <54848194+btwiuse@users.noreply.github.com>
2023-12-01 20:47:29 +0000

    test passing byte slices

--- lib.rs
+++ lib.rs
@@ -2,10 +2,14 @@ use wasm_bindgen::prelude::*;
 
 /// add two u128 numbers
 #[wasm_bindgen]
-pub fn add(a: &str, b: &str) -> String {
-  let a_num = a.parse::<u128>().unwrap();
-  let b_num = b.parse::<u128>().unwrap();
-  (a_num + b_num).to_string()
+pub fn add(a: &[u8], b: &[u8]) -> Vec<u8> {
+  let mut a_slice = [0u8; 16];
+  let mut b_slice = [0u8; 16];
+  a_slice.copy_from_slice(a);
+  b_slice.copy_from_slice(b);
+  let a_num = u128::from_le_bytes(a_slice);
+  let b_num = u128::from_le_bytes(b_slice);
+  (a_num + b_num).to_le_bytes().to_vec()
 }
 
 #[cfg(test)]
@@ -14,7 +18,9 @@ mod tests {
 
   #[test]
   fn it_works() {
-    let result = add("1", "2");
-    assert_eq!(result, "3");
+    let a = 1u128.to_le_bytes();
+    let b = 2u128.to_le_bytes();
+    let result = add(&a, &b);
+    assert_eq!(result, 3u128.to_le_bytes().to_vec());
   }
 }

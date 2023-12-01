b056382e6b2ae8344aaec3e8a1a2dd0238a710b6
btwiuse <54848194+btwiuse@users.noreply.github.com>
2023-12-01 20:19:40 +0000

    fix cargo test

--- lib.rs
+++ lib.rs
@@ -2,7 +2,7 @@ use wasm_bindgen::prelude::*;
 
 /// add two u32 numbers
 #[wasm_bindgen]
-pub fn add(a: String, b: String) -> String {
+pub fn add(a: &str, b: &str) -> String {
   let a_num = a.parse::<u32>().unwrap();
   let b_num = b.parse::<u32>().unwrap();
   (a_num + b_num).to_string()
@@ -14,7 +14,7 @@ mod tests {
 
   #[test]
   fn it_works() {
-    let result = add(1, 2);
-    assert_eq!(result, 3);
+    let result = add("1", "2");
+    assert_eq!(result, "3");
   }
 }
